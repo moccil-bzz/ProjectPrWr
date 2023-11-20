c3d0f8b12c7f4b96a276704dafba6afdd36b7be7
moccil-bzz <moccil@bzz.com>
2023-11-20 11:57:12 +0100

    added dark theme to dropdown, added language dropdown and fixed window resizing problem

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", content_loader);
 
 function content_loader(){
-
+    document.getElementById("themeBtn-light").addEventListener("click", changeThemeLight);
+    document.getElementById("themeBtn-dark").addEventListener("click", changeThemeDark);
 }
 
+function changeThemeLight() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+}
+
+function changeThemeDark() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+}
