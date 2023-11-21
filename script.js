d6f4e6ec98ede3a221b34bbc6baff9f64f8c8ccc
moccil-bzz <moccil@bzz.com>
2023-11-21 12:34:51 +0100

    added darktheme to dropdown

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ function changeThemeLight() {
     document.getElementsByTagName("body")[0].style.backgroundColor = "white";
     document.getElementById("credits-left").src = './img/credits%20left%20transp%201%20og.png';
     document.getElementById("credits-right").src = './img/credits%20right%20transp%201%20og.png';
+    document.querySelectorAll(".dropdown-darkTheme").forEach((i) => {
+        i.classList.remove("dropdown-darkTheme");
+        i.classList.add("dropdown-lightTheme");
+    });
 }
 
 function changeThemeDark() {
     document.getElementsByTagName("body")[0].style.backgroundColor = "black";
     document.getElementById("credits-left").src = './img/credits%20left%20transp%20og%201%20dm.png';
     document.getElementById("credits-right").src = './img/credits%20right%20transp%20og%201%20dm.png';
+    document.querySelectorAll(".dropdown-lightTheme").forEach((i) => {
+        i.classList.remove("dropdown-lightTheme")
+        i.classList.add("dropdown-darkTheme");
+    });
 }
