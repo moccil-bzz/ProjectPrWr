d276f15159a9893e372ae092269d790c3ce5d636
marko <pereiram@bzz.ch>
2023-11-21 10:52:35 +0100

    Added 2 new images, white versions of the credits for darkmode. I wrote some js too, but I can't get it to work

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ function content_loader(){
 
 function changeThemeLight() {
     document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+    document.getElementById("credits-left").src = './img/credits%20left%20transp%201%20og.png';
+    document.getElementById("credits-right").src = './img/credits%20right%20transp%201%20og.png';
 }
 
 function changeThemeDark() {
     document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+    document.getElementById("credits-left").src = './img/credits%20left%20transp%20og%201%20dm.png';
+    document.getElementById("credits-right").src = './img/credits%20right%20transp%20og%201%20dm.png';
 }
